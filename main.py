50693cb95bfa7e5db1183d5a89e6afd6418b97d3
Jason-Cee <jasoncee017@gmail.com>
2021-05-20 15:56:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#  Basic Calculator
+
+class Add:
+    def __init__(self):
+        pass
+
+    def __add__(self, a, b):
+        return a + b
+
+
+class Subtract:
+    def __init__(self):
+        pass
+
+    def subtract(self, a, b):
+        return a - b
+
+
+class Division:
+    def __init__(self):
+        pass
+
+    def divide(self, a, b):
+        return a / b
+
+class Multiplication:
+    def __init__(self):
+        pass
+    def multiply(self, a, b):
+        return a * b
